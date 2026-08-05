c57bfbe51c274727ea8214f37e133b519da37e87
and0d0 <and0d0@users.noreply.github.com>
2026-08-05 10:15:16 +0800

    feat(ptodsl): add multi-tile allocation helpers

--- ptodsl/ptodsl/pto.py
+++ ptodsl/ptodsl/pto.py
@@ -119,7 +119,7 @@ from ._ops import (             # noqa: F401
     vmula, vmadd,
     vsel,
     make_tensor_view, partition_view,
-    alloc_buffer, alloc_tile,
+    alloc_buffer, alloc_tile, alloc_multi_tile, multi_tile_get,
     tsort32, tmrgsort, tgather, tscatter,
     mte_load, mte_store, mte_gm_ub, mte_ub_gm, mte_ub_ub, mte_ub_l1,
     mte_gm_l1, mte_l1_ub, mte_gm_l1_frac, mte_l1_bt, mte_l1_fb, mem_bar,
